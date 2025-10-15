b3382ed3722e7a16ee7c94a79f39d0f7d319c227
Ruan Federle <ruanfederle@hotmail.com>
2025-10-15 19:32:00 -0400

    Adiciona function findEmployeeBySpecialization

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java' },
       //... More employee records can be added here
     ];
 
@@ -34,4 +34,13 @@ function findEmployeeById(employeeId) {
      }
  }
 
- 
+ function findEmployeeBySpecialization(employeeSpecialization) {
+    const foundEmployeeSpecialization = employees.find(employee => employee.specialization === employeeSpecialization);
+    if (foundEmployeeSpecialization) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployeeSpecialization.id}: ${foundEmployeeSpecialization.name} - ${foundEmployeeSpecialization.department} - ${foundEmployeeSpecialization.specialization} - $${foundEmployeeSpecialization.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this Specialization';
+     }
+ }
+
